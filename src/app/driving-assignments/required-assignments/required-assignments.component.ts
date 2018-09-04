9aaa6f421de16722b4f396e81192b8211e434601
Changhui Xu <changhuixu@hotmail.com>
2018-09-03 22:47:04 -0500

    add a sample page
    show the effect of vertical scroll page

--- src/app/driving-assignments/required-assignments/required-assignments.component.ts
+++ src/app/driving-assignments/required-assignments/required-assignments.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Car {
+  make: string;
+  model: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-required-assignments',
   templateUrl: './required-assignments.component.html',
   styleUrls: ['./required-assignments.component.css']
 })
 export class RequiredAssignmentsComponent implements OnInit {
+  cars: Car[] = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 }
+  ];
   constructor() {}
 
   ngOnInit() {}
