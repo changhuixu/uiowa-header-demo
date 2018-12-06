206180a094d12f73790bb8c0a18cb521deb471c9
Changhui Xu <changhuixu@hotmail.com>
2018-12-06 09:59:31 -0600

    auto collapse navbar toggler when click nav link

--- projects/uiowa/uiowa-header/src/lib/uiowa-app-nav-bar/uiowa-app-nav-bar.component.ts
+++ projects/uiowa/uiowa-header/src/lib/uiowa-app-nav-bar/uiowa-app-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, HostListener, ElementRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InternalRoute } from '../models/internal-route';
 
 @Component({
@@ -10,16 +10,5 @@ export class UiowaAppNavBarComponent {
   @Input() applicationName: string;
   @Input() internalRoutes: InternalRoute[];
   isCollapsed = true;
-  constructor(private elRef: ElementRef) {}
-
-  @HostListener('document:click', ['$event'])
-  @HostListener('document:touchstart', ['$event'])
-  handleOutsideClick(event: MouseEvent | TouchEvent) {
-    if (this.isCollapsed) {
-      return;
-    }
-    if (!this.elRef.nativeElement.contains(event.target)) {
-      this.isCollapsed = true;
-    }
-  }
+  constructor() {}
 }
