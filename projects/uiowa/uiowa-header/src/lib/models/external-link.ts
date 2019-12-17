fb4a602a1d52556ded64294fcfdab9ff8bbdb1d9
Adam Clemence <adam-clemence@uiowa.edu>
2019-12-17 15:17:19 -0600

    Add option to set the target for external links, default to _blank

--- projects/uiowa/uiowa-header/src/lib/models/external-link.ts
+++ projects/uiowa/uiowa-header/src/lib/models/external-link.ts
@@ -1,3 +1,3 @@
 export class ExternalLink {
-  constructor(public text: string, public url: string) {}
+  constructor(public text: string, public url: string, public target: "_blank" | "_self" | "_parent" | "_top" = "_blank") {}
 }
